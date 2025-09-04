084468aa830c8e38b15cc87a298567e302d322aa
carlos mario toro martinez <carlostorom.96@gmail.com>
2025-09-03 23:21:42 -0400

    feature: added changes of database in diff domains

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ router(app);
 // app.get("/", (req, res) => {
 //   res.send("¡Welcome!");
 // });
-
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "dist", "index.html"));
 // });
--- middlewares/selectedPharmacy.js
+++ middlewares/selectedPharmacy.js
@@ -2,8 +2,12 @@ const db = require("../libs/dbConexionORM");
 const configs = require("../config/databases.json");
 
 module.exports = (req, res, next) => {
-  const host = req.headers.host.split(":")[0];
+  //const host = req.headers.host.split(":")[0];
+  const host = req.headers.origin;
+
   try {
+    console.log(req.headers.origin);
+
     db.setConnectionByDomain(host, configs);
     next();
   } catch (err) {
